9f7907d1fd7e2a3a329345877ac3aa128a36ed91
Brahim Khayya <brahim.jarrachi_khayya@etudiant.univ-lr.fr>
2024-04-01 13:59:15 +0200

    added artwork fetch from spotify api

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -3,6 +3,7 @@ import { getServerSession } from 'next-auth'
 import { notFound, redirect } from 'next/navigation'
 import spotifyApi from '@/utils/spotify'
 import axios from 'axios'
+import Image from 'next/image'
 
 
 
@@ -19,7 +20,7 @@ async function getServerSideProps() {
     },
     data: 'grant_type=client_credentials'
   };
-
+  console.log(authOptions);
   const response = await axios(authOptions);
   const accessToken = response.data.access_token;
 
@@ -34,15 +35,29 @@ async function getData(){
 }
 //fonction pour tester l'api (ce n'est pas ce que cherche à faire)
 async function getTrack(id, accessToken){
-  const res = await fetch(`https://api.spotify.com/v1/tracks/${id}`, {
-    headers: {
-      'Authorization': `Bearer ${accessToken}`
-    }
-  });
-  if(!res.ok) return notFound();
-  return res.json();
+  try{
+    const res = await fetch(`https://api.spotify.com/v1/tracks/${id}`, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+    if(!res.ok) return notFound();
+    return res.json();
+  }catch(e){
+    console.log(e);
+  }
+  
 }
 
+async function getTrackImage(id, accessToken){
+  try{
+    const res = await getTrack(id, accessToken);
+    if(res === null) return notFound();
+    return res.album.images[0].url;
+  }catch(e){
+    console.log(e);
+  }
+}
 
 
 
@@ -59,21 +74,22 @@ const Dashboard = async () => {
 
   const { accessToken } = await getServerSideProps();
 
-
+  //const track = await getTrack('0gNNToCW3qjabgTyBSjt3H', accessToken);
+  //console.log('image url here:', track.album.images[0].url);
 
   return (
     // afficher les chansons de l'utilisateur
-
+    
     <div>
-      {data && data?.map((song) => (
+      {data && data?.map(async (song) => (
         <div key={song._id}>
           <h1>{song.id}</h1>
           <h1>{song.name}</h1>
           <h1>{song.artist}</h1>
           <h1>{song.duration_ms}</h1>
 
-          <h1>Track</h1>
-          <h1>{getTrack(song.id, accessToken)}</h1>
+          <Image src={await getTrackImage(song.id, accessToken)} width = {200} height= {200} alt="not found"/>
+          <h1></h1>
 
 
         </div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        // We add it to the remote pattern for the static images we use from GitHub
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'i.scdn.co',
+            port: '',
+            pathname: '/image/**',
+          }
+        ]
+    }
+};
 
 export default nextConfig;
